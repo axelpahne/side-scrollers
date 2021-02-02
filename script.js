d247fe74e985c39a2c03ddaa74f940ea589f1cc1
Ant1N <71013483+Ant1N@users.noreply.github.com>
2021-02-02 13:25:50 +0100

    Comment JS-code

--- script.js
+++ script.js
@@ -43,20 +43,21 @@ gameArea();
 //     ctx.fillRect(0, 0, 150, 75);
 // }​​​​​
 
-
+//Create canvas-area where game will be played
 function gameArea() {
-    let myGameArea = document.createElement("canvas");
-    context = myGameArea.getContext('2d');
-    document.body.insertAdjacentElement('afterbegin', myGameArea);
-    myGamePiece = new Image();
-    myGamePiece.src = "./images/rymdtesla100px.png";
-    myGamePiece.onload = function() {
-        context.drawImage(myGamePiece, 0, 50, 10, 10);
+    let myGameArea = document.createElement("canvas"); //create element "canvas" 
+    context = myGameArea.getContext('2d'); //okänt vad det gör men verkar behövas
+    document.body.insertAdjacentElement('afterbegin', myGameArea); //insert "canvas" to HTML
+    myGamePiece = new Image(); //Create gamepiece (space tesla)
+    myGamePiece.src = "./images/rymdtesla100px.png"; //Get the spacetesla img src
+    myGamePiece.onload = function() { //onload function
+        context.drawImage(myGamePiece, 0, 50, 10, 10); //drawimage(show img) on canvas (x-axis, y-axis, width, height)
     };
 }
 
-
-function moveup() {
+//Temporary button function
+//Link to myGamePiece?
+/*function moveup() {
     myGamePiece.speedY = -1; 
 }
 
@@ -75,7 +76,7 @@ function moveright() {
 function clearMove() {
     myGamePiece.speedX = 0; 
     myGamePiece.speedY = 0; 
-}
+}*/
 
 
 
