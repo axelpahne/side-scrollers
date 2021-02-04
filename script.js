bd20c314ca1fd6beda4c63c6ce35f9635cf80a57
Ant1N <isakantin@live.se>
2021-02-04 13:52:55 +0100

    more styling and CSS link solution

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ let spawnRate = 1500;
 // Checks when to start score count
 let scoreCountStar = false;
 // Checks when to start score count
-let winGameScore = 100;
+let winGameScore = 20000;
 
 
 //Create all images
